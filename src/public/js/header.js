c41909ad748787fbe380951600f174b596690367
Tobias Weihing <tobi.wei.handy@gmail.com>
2025-02-11 10:36:18 +0100

    fix: CSS headerNav

--- src/public/js/header.js
+++ src/public/js/header.js
@@ -2,3 +2,10 @@ function toggleMenu() {
     const menu = document.querySelector('.nav_left');
     menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
 }
+
+// Reset-Menü wenn Bildschirmgröße geändert wird
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 768) {
+        document.getElementById('menu-checkbox').checked = false;
+    }
+});
