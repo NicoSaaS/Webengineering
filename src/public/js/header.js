ca71845485452ca5cdb6d7f4d84b6b3f86bd1e2d
Hannes Klas <hannes.klas@sap.com>
2025-02-12 09:20:41 +0100

    Dropdown fix

--- src/public/js/header.js
+++ src/public/js/header.js
@@ -3,7 +3,6 @@ function toggleMenu() {
     menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
 }
 
-// Reset-Menü wenn Bildschirmgröße geändert wird
 window.addEventListener('resize', function() {
     if (window.innerWidth > 768) {
         document.getElementById('menu-checkbox').checked = false;
